a92fb7c8986e14f51599235f4f31ed9acf0b4087
prefer_youn <38209666+shyoun94@users.noreply.github.com>
2024-03-18 18:12:14 +0900

    Next.js 튜토리얼 예시

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+    <h2>Welcome</h2>
+    Hello, WEB
+    <p><img src="/hello.png" width="80" alt="" /></p>
+    </>
+  )
+}
---
+++ src/app/read/[id]/page.js
@@ -0,0 +1,13 @@
+export default async function Read(props) {
+  const id = props.params.id;
+  const resp = await fetch(`http://localhost:9999/topics/${id}`, {
+    cache: "no-cache",
+  });
+  const topic = await resp.json();
+  return (
+    <>
+      <h2>{topic.title}</h2>
+      {topic.body}
+    </>
+  );
+}
